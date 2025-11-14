19031d1b200b0c345b14ff6a587ac7e1c9ab0ee1
Florian <103135093+MoonUnicorne@users.noreply.github.com>
2025-11-14 13:39:17 +0000

    Ajout du namespace 'articles' aux URLs et correction des liens dans les templates

--- articles/urls.py
+++ articles/urls.py
@@ -19,6 +19,8 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+app_name = 'articles'
+
 urlpatterns = [
     # path("admin/", admin.site.urls),
     path('', views.articles_view, name='Articles'),
