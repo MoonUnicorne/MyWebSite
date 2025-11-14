ad8f85efc24f2ed3518d940ef011f7b6edb98e43
Florian <103135093+MoonUnicorne@users.noreply.github.com>
2025-11-14 14:14:18 +0000

    Ajout du modèle 'Article' avec ses champs dans les migrations et le fichier models.py

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    titre = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=100,unique=True)
+    contenu = models.TextField()
+    datePublication = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titre
