7b63eede4ab12d42ed67334fc5ab2781d48cfb40
Florian Eckert <flo.eck@hotmail.com>
2025-11-21 16:23:59 +0100

    Changement tests

--- app/tests.py
+++ app/tests.py
@@ -1,4 +1,4 @@
-"""
+﻿"""
 This file demonstrates writing tests using the unittest module. These will pass
 when you run "manage.py test".
 """
@@ -20,15 +20,21 @@ class ViewTest(TestCase):
 
     def test_home(self):
         """Tests the home page."""
+        print("Test page home")
         response = self.client.get('/')
+        print("Response:", response)
         self.assertContains(response, 'Home Page', 1, 200)
 
-    def test_contact(self):
-        """Tests the contact page."""
-        response = self.client.get('/contact')
-        self.assertContains(response, 'Contact', 3, 200)
+    # def test_contact(self):
+    #     """Tests the contact page."""
+    #     print("Test page contact")
+    #     response = self.client.get('/contact')
+    #     print("Response:", response)
+    #     self.assertContains(response, 'Contact', 3, 200)
 
-    def test_about(self):
-        """Tests the about page."""
-        response = self.client.get('/about')
-        self.assertContains(response, 'About', 3, 200)
+    # def test_about(self):
+    #     """Tests the about page."""
+    #     print("Test page about")
+    #     response = self.client.get('/about')
+    #     print("Response:", response)
+    #     self.assertContains(response, 'About', 3, 200)
