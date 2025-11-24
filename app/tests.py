8e4cc030c2c780fd2b4d9bd8affa2a66079c93b4
Florian Eckert <flo.eck@hotmail.com>
2025-11-24 14:54:04 +0100

    Remise en place test page Contact, About et ajout test CV

--- app/tests.py
+++ app/tests.py
@@ -20,21 +20,20 @@ class ViewTest(TestCase):
 
     def test_home(self):
         """Tests the home page."""
-        print("Test page home")
         response = self.client.get('/')
-        print("Response:", response)
         self.assertContains(response, 'Home Page', 1, 200)
 
-    # def test_contact(self):
-    #     """Tests the contact page."""
-    #     print("Test page contact")
-    #     response = self.client.get('/contact')
-    #     print("Response:", response)
-    #     self.assertContains(response, 'Contact', 3, 200)
+    def test_contact(self):
+        """Tests the contact page."""
+        response = self.client.get('/contact', follow=True)
+        self.assertContains(response, 'Contact', 3, 200)
 
-    # def test_about(self):
-    #     """Tests the about page."""
-    #     print("Test page about")
-    #     response = self.client.get('/about')
-    #     print("Response:", response)
-    #     self.assertContains(response, 'About', 3, 200)
+    def test_about(self):
+        """Tests the about page."""
+        response = self.client.get('/about', follow=True)
+        self.assertContains(response, 'About', 3, 200)
+
+    def test_cv(self):
+        """Tests the cv page."""
+        response = self.client.get('/cv', follow=True)
+        self.assertContains(response, 'CV', 3, 200)
