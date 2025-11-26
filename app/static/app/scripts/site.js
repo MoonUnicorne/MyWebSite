da174c4b92563a41b0af3d2bf9a1e2a259b9b33f
Florian Eckert <flo.eck@hotmail.com>
2025-11-26 14:34:59 +0100

    Fixage navbar, ajout bouton switch theme

---
+++ app/static/app/scripts/site.js
@@ -0,0 +1,9 @@
+﻿document.getElementById('btnSwitch').addEventListener('click', () => {
+    console.log('Switching theme')
+    if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+        document.documentElement.setAttribute('data-bs-theme', 'light')
+    }
+    else {
+        document.documentElement.setAttribute('data-bs-theme', 'dark')
+    }
+})
