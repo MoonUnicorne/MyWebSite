67e7ba6df3cdb4869fa82f0113a5a0e9aef66b70
Florian Eckert <flo.eck@hotmail.com>
2025-12-02 13:46:18 +0100

    update settings

--- MyDjangoWebsite/settings.py
+++ MyDjangoWebsite/settings.py
@@ -68,15 +68,11 @@ TEMPLATES = [
     },
 ]
 
-TEMPLATE_DIRS = [
-    os.path.join(BASE_DIR, 'templates'),
-]
 
 print("TEMPLATES DIRS =", ' ; '.join(TEMPLATES[0]["DIRS"]))
-print("TEMPLATE_DIRS =", ' ; '.join(TEMPLATE_DIRS))
 
 
-WSGI_APPLICATION = "blog.wsgi.application"
+WSGI_APPLICATION = "MyDjangoWebsite.wsgi.application"
 
 
 # Database
