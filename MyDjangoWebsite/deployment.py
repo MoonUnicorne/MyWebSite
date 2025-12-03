9c56340ef8bee1f19b998da5ec839f4a0e80051f
Florian Eckert <flo.eck@hotmail.com>
2025-12-03 11:57:37 +0100

    adobueidoeuixxkjqopy.i

--- MyDjangoWebsite/deployment.py
+++ MyDjangoWebsite/deployment.py
@@ -69,7 +69,7 @@ WSGI_APPLICATION = "MyDjangoWebsite.wsgi.application"
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('DBNAME'),
         'HOST': os.environ.get('DBHOST'),
         'USER': os.environ.get('DBUSER'),
